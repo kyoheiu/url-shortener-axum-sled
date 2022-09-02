3cedc106f38eb81b695ab6caca784700c5f8d3d2
Kyohei Uto <kyoheiu@outlook.com>
2022-09-03 06:31:21 +0900

    Return uuid from string request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod router;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use router::*;
 
 #[tokio::main]
@@ -8,7 +11,9 @@ async fn main() {
     env_logger::init();
     println!("Server started.");
 
-    let app = Router::new().route("/", get(health));
+    let app = Router::new()
+        .route("/", get(health))
+        .route("/shorten", post(shorten));
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
--- src/router.rs
+++ src/router.rs
@@ -4,3 +4,9 @@ use axum::debug_handler;
 pub async fn health() -> String {
     "Hello, developer.".to_string()
 }
+
+#[debug_handler]
+pub async fn shorten(body: String) -> String {
+    let uuid = nanoid::nanoid!(8);
+    return uuid;
+}
